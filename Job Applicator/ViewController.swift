a090da278c7cbe5f94fd941283699dd2a161ed9a
Terence Baker <terence@bulbstudios.com>
2019-02-27 10:33:58 +0000

    Initial commit. Pods workspace created

---
+++ Job Applicator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Job Applicator
+//
+//  Created by Terence Baker on 2019-02-27.
+//  Copyright © 2019 Bulb Studios Ltd. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '11.0'
+
+target 'Job Applicator' do
+
+  use_frameworks!
+
+end
